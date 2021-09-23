92eb3935a66a9d652d48d03ad9b4503197d0ab5c
rpoxo <irpoxotaujio@gmail.com>
2021-09-23 18:41:30 +0300

    add templates load

--- src/merge_group.py
+++ src/merge_group.py
@@ -10,28 +10,8 @@ from operator import attrgetter
 import bf2mesh
 from bf2mesh.visiblemesh import VisibleMesh
 
+from mod import get_mod_templates, get_mod_geometries
 from staticobject import Staticobject, parse_config_staticobjects
-
-def get_mod_meshes(modroot):
-    pattern_geometry_create = r'GeometryTemplate.create StaticMesh (?P<filename>\S+)'
-    meshes: Dict[str, str] = {}
-    scanpath = os.path.join(modroot, 'objects')
-    for dirname, dirnames, filenames in os.walk(scanpath):
-        for filename in filenames:
-            if filename.endswith('.con'):
-                confile = os.path.join(scanpath, dirname, filename)
-                with open(confile, 'r') as config:
-                    # GeometryTemplate.create StaticMesh afghannorth_stairs2patioflat
-                    staticmeshes = re.findall(pattern_geometry_create, config.read())
-                    if staticmeshes:
-                        for staticmesh in staticmeshes:
-                            meshpath = os.path.join(scanpath, dirname, 'meshes', staticmesh+'.staticmesh')
-                            if not os.path.exists(meshpath):
-                                message = f'Missing mesh {meshpath}'
-                                #raise FileNotFoundError(message)
-                                continue
-                            meshes[staticmesh] = meshpath
-    return meshes
 
 def get_groups(staticobjects):
     return [list(group) for _, group in groupby(sorted(staticobjects, key=attrgetter('group')), attrgetter('group'))]
@@ -118,8 +98,9 @@ def main():
     levelname = 'kokan'
     config_group = 'StaticObjects_2.con'
 
-    geometries = get_mod_meshes(modroot)
-    merge_group(modroot, levelname, config_group, geometries)
+    geometries = get_mod_geometries(modroot)
+    templates = get_mod_templates(modroot)
+    merge_group(modroot, levelname, config_group, templates, geometries)
 
     # group objects in editor by mapper
     # generate merge plan:
--- src/mod.py
+++ src/mod.py
@@ -1,17 +1,17 @@
 import os
 import re
+import logging
 from typing import Dict
 
-def get_mod_geometries(root, modPath):
+def get_mod_geometries(modroot):
     pattern_geometry_create = r'GeometryTemplate.create StaticMesh (?P<filename>\S+)'
     meshes: Dict[str, str] = {}
-    scanpath = os.path.join(root, modPath, 'objects')
+    scanpath = os.path.join(modroot, 'objects')
     for dirname, dirnames, filenames in os.walk(scanpath):
         for filename in filenames:
             if filename.endswith('.con'):
                 confile = os.path.join(scanpath, dirname, filename)
                 with open(confile, 'r') as config:
-                    # GeometryTemplate.create StaticMesh afghannorth_stairs2patioflat
                     staticmeshes = re.findall(pattern_geometry_create, config.read())
                     if staticmeshes:
                         for staticmesh in staticmeshes:
@@ -19,6 +19,22 @@ def get_mod_geometries(root, modPath):
                             if not os.path.exists(meshpath):
                                 message = f'Missing mesh {meshpath}'
                                 #raise FileNotFoundError(message)
+                                logging.warning(message)
                                 continue
                             meshes[staticmesh] = meshpath
     return meshes
+
+def get_mod_templates(modroot):
+    pattern_template_create = r'ObjectTemplate.create (?P<ObjectType>\S+) (?P<filename>\S+)'
+    templates: Dict[str, str] = {}
+    scanpath = os.path.join(modroot, 'objects')
+    for dirname, dirnames, filenames in os.walk(scanpath):
+        for filename in filenames:
+            if filename.endswith('.con'):
+                confile = os.path.join(scanpath, dirname, filename)
+                with open(confile, 'r') as config:
+                    templates = re.findall(pattern_template_create, config.read())
+                    if templates:
+                        for template in templates:
+                            templates[template] = confile
+    return templates
